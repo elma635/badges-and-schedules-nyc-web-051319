6df74daf91d26f29ac1b94df02ca8fe51c53c783
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 00:22:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,33 +7,3 @@ def batch_badge_creator(attendees)
     badge_maker(attendees)
 end
 
-#takes an array of names as an argument and returns an array of badge messages.
-#what badge msgs? if the badge_maker is doing that alrdy.
-
-
-def assign_rooms(room_assignments)
-  room_assignments.each do | room_assignments |
-
-  #puts "Hello, #{speaker}! You'll be assigned to room #{room_assignments}!"
-
-  (1..7).each.with_index(1) do |speaker, index|
-      puts "Hello, #{speaker}! You'll be assigned to room #{index}"
-  end
-
-  #if this is an array -- how can you print out each assgnmnt? -- need to
-  #iterate over your array of room assgnmnt in order to puts out individual assgnmnt.
-
-  #assign rm# to each person
-
-
-
-
-
-#def printer(list_of_badges, room_assignments)
-
-  #puts
-  #should puts list of badges and room_assignments
-
-#outputs result of batch_badge_creato
-#outputs result of assign_rooms
-#end
