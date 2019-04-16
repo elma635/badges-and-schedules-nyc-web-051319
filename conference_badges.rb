86d42f1339a607034dec1858c2afc5fbed15c12a
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 00:27:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 end
 
 def assign_rooms(speaker, room_number)
- speaker.collect.each_with_index do |speaker, room_number|
+ speaker.collect.room_number.each_with_index do |speaker, room_number|
    "Hello, #{speaker}! You will be assigned room number #{room_number + 1}!"
 end
 end
